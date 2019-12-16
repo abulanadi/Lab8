9cf82ef6a1f064f6fedccffb0d10df731cf571ee
abulanadi <42947607+abulanadi@users.noreply.github.com>
2019-12-15 20:35:55 -0800

    Add project files.

---
+++ MatrixGen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+	// This class encompasses my matrix generation methods
+	class MatrixGen
+	{
+	}
+}
